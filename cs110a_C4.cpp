8d8a62752e270119bdf272691c1aced20e5c09d2
Fatima Zahra Chriha <30349896+FatiFleurCH@users.noreply.github.com>
2019-10-16 15:10:26 -0400

    Create cs110a_C4.cpp

---
+++ cs110a_C4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main{
+	int num1,num2;
+	cout << "type number1 and number2";
+	cin >> num1 >> num2;
+  
+  num1 >= num2 ? cout << "the max is: " << num1 << "the min is: " << num2: cout << "the max is: " << num2 << "the min is: " << num1
+
+	return 0;
+}
+
+
